a51efd94b4eb45efd5147898ffe2e8aa471e9737
Andrei <a.alfonso@bitcoinwell.com>
2025-11-05 11:04:11 -0500

    feat: git lens + tab title

--- init.lua
+++ init.lua
@@ -18,3 +18,13 @@ require("lspconfig").typos_lsp.setup({
 -- Enable debug logs for the LSP client. Recommended for debugging only.
 -- vim.lsp.set_log_level("debug")
 vim.lsp.set_log_level("error")
+
+-- Enable setting the terminal title
+vim.opt.title = true
+
+-- Set the format for the title
+-- %{getcwd()} is a function that gets the current working directory
+vim.opt.titlestring = "nvim - %{fnamemodify(getcwd(), ':~')}"
+
+-- set a title to restore when you exit Neovim
+-- vim.opt.titleold = "ghostty"
